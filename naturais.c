f0186e2fd8f90196bea241d080dd9eafe04bb0cc
Rafael Takano <rafael-takano@hotmail.com>
2018-03-30 17:23:10 -0300

    naturais recursivo

---
+++ naturais.c
@@ -0,0 +1,21 @@
+    #include <stdio.h>
+    #include <stdlib.h>
+    //
+    int aux = 0;
+    int naturais(int numero){
+        if(aux < numero){
+            aux = aux + 1;
+            printf("%d ",aux);
+            return(naturais(numero));
+        }
+        else{
+            return(EXIT_SUCCESS);
+        }
+    }
+    //
+    int main(){
+        int entrada;
+        scanf("%d",&entrada);
+        naturais(entrada);
+        return(EXIT_SUCCESS);
+    }
