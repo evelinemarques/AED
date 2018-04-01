cacbdc0a267809e5937d9b35d5c0af385543f477
Rafael Takano <rafael-takano@hotmail.com>
2018-04-01 16:23:05 -0300

    naval recursivo

--- naval.c
+++ naval.c
@@ -1,24 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int verifica(int *matriz, int numero, int aux){
-
-}
-
-int main(){
+void main(){
     char **matriz;
-    int m,n; //ex: 5 e 3 ou 5 e 5//
-    int i,j;
-    scanf("%d %d",&m,&n);
-    //alocando uma matriz de ponteiro//
-    matriz = (char*)malloc(m*sizeof(char));
-    for(i = 0; i < m; i++){
-        matriz[i] = (char*)malloc(n*sizeof(char));
-    }
-    //percorrendo a matriz com os valores . ou #
+    int m,n;
+    int jogadas;
+    int i;
+    //capturando o numero de linhas e colunas para matriz//
+    scanf("%d",&m);
+    scanf("%d",&n);
+    //alocando memoria para matriz de char//
+    matriz = malloc(m * sizeof(char));
     for(i=0; i<m; i++){
-        for(j=0; j<n; j++){
-            scanf("%c",&matriz[i][j]);
-        }
+        matriz[i] = malloc(n * sizeof(char));
     }
 }
