9aa93ed2c399c27d19c6b69622dde34e3637375a
Rafael Takano <rafael-takano@hotmail.com>
2018-04-01 16:46:15 -0300

    naval recursivo

--- naval.c
+++ naval.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void main(){
+int cont = 0;
+
+int verifica(char **matriz, int numero, int aux){
+    int p,q;
+    scanf("%d",&p);
+    scanf("%d",&q);
+    if(matriz[p-1][q-1] == '#'){
+        cont++;
+    }
+    //
+    if(aux < numero){
+        aux++;
+        return(verifica(matriz,numero,aux));
+    }
+    else{
+        return(printf("%d\n",cont));
+    }
+}
+
+int main(){
     char **matriz;
-    int m,n;
-    int jogadas;
-    int i;
+    int m,n; //tamanho da matriz//
+    int jogadas; //numero de jogadas//
+    int aux = 1; //contador aux de jogadas feitas//
+    int i,j;
     //capturando o numero de linhas e colunas para matriz//
     scanf("%d",&m);
     scanf("%d",&n);
@@ -14,4 +34,24 @@ void main(){
     for(i=0; i<m; i++){
         matriz[i] = malloc(n * sizeof(char));
     }
+    //preencher a matriz de chars '.' ou '#'//
+    for(i=0; i<m; i++){
+        for(j=0; j<n; j++){
+            scanf(" %c",&matriz[i][j]);
+        }
+    }
+    /*mostrando a matriz
+    for(i=0; i<m; i++){
+        for(j=0; j<n; j++){
+            printf("m[%d][%d] = %c\n",i,j,matriz[i][j]);
+        }
+    }*/
+    //pegando o numero de jogadas//
+    scanf("%d",&jogadas);
+    //verificando//
+    verifica(matriz,jogadas,aux);
+    //
+    free(matriz);
+    matriz = NULL;
+    return(EXIT_SUCCESS);
 }
